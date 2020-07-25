2f2b216873ba48eac67ba06fce329cd6a20ec680
william.yang <william.yang@kopsoft.cn>
2020-07-25 23:46:20 +0800

    初始

---
+++ src/Opc.Net.Demo/FrmMain.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Timers;
+using System.Windows.Forms;
+using Opc.Net;
+
+namespace Opc.Net.Demo
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        string barcode;
+
+
+        private OpcManager opcManager;
+        private System.Timers.Timer opcTimer;
+        private int[] pErrors;
+        private Dictionary<int, object> items;
+        /// <summary>
+        /// 写入采煤机位置数据
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            items = new Dictionary<int, object>();
+            items.Add(2, textBox2.Text);
+            //opcManager.Write(items, 1,  pErrors);
+            opcManager.Write(items, 1, out pErrors);
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            opcManager = new OpcManager(AppDomain.CurrentDomain.BaseDirectory + "\\Opc.config.xml");
+            opcManager.OnReadCompleted += new EventHandler<OpcDaCustomAsyncEventArgs>(opcManager_OnReadCompleted);
+
+            opcTimer = new System.Timers.Timer()
+            {
+                Interval = 100,
+                AutoReset = true,
+                Enabled = true
+            };
+            opcTimer.Elapsed += new ElapsedEventHandler(opcTimer_Elapsed);
+        }
+
+        void opcTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            opcManager.Read();
+        }
+
+        void opcManager_OnReadCompleted(object sender, OpcDaCustomAsyncEventArgs e)
+        {
+            //Invoke((ThreadStart)(() =>
+            //{
+
+            //    if (OpcHelper.ShowValue(e, 3) != null)
+            //    {
+            //        textBox1.Text = OpcHelper.ShowValue(e, 3).ToString();
+            //    }
+            //}));
+
+
+
+            Invoke((ThreadStart)(() =>
+            {
+                this.textBox1.Clear();
+                if (OpcHelper.ShowValue(e, 1) != null)
+                {
+                    short[] b = (short[])OpcHelper.ShowValue(e, 1);
+                    foreach (var item in b)
+                    {
+                        this.textBox1.AppendText(item.ToString() + ",");
+                    }
+                }
+            }));
+
+
+            barcode = this.textBox1.Text;
+
+
+            //string sql = $"INSERT into barcode(barcode,Var01,Var02,Var03,Var04,Var05,Var06,Var07,Var08,CreateTime,IsPrinted) values('{barcode}','{Var01}','{Var02}','{Var03}','{Var04}','{Var05}','{Var06}','{Var07}','{Var08}',0,0)";
+
+
+            string sql = $"INSERT into barcode(barcode) values('{barcode}')";
+
+            succeed = SqlSugarHelper.DB.Ado.ExecuteCommand(sql);
+
+
+        }
+        int succeed = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+
+
+
+            //this.textBox1.AppendText(item.ToString() + ",");
+
+
+            //Invoke((ThreadStart)(() =>
+            //{
+            //    this.textBox1.Clear();
+            //    if (OpcHelper.ShowValue(e, 1) != null)
+            //    {
+            //        short[] b = (short[])OpcHelper.ShowValue(e, 1);
+            //        foreach (var item in b)
+            //        {
+            //            this.textBox1.AppendText(item.ToString() + ",");
+            //        }
+            //    }
+            //}));
+
+            //barcode = this.textBox1.Text;
+
+
+            string a = barcode;
+
+            string sql = $"INSERT into barcode(barcode) values('{barcode}')";
+
+
+
+            succeed = SqlSugarHelper.DB.Ado.ExecuteCommand(sql);
+        }
+    }
+}
---
+++ src/Opc.Net.Demo/SqlSugarHelper.cs
@@ -0,0 +1,29 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opc.Net.Demo
+{
+    public class SqlSugarHelper
+    {
+        private static string conn = $"Data Source={AppDomain.CurrentDomain.BaseDirectory}DB\\KopSoftSCADA.db;Version=3";
+
+        public static SqlSugarClient DB
+        {
+            get => new SqlSugarClient(new ConnectionConfig()
+            {
+                ConnectionString = conn,
+                DbType = DbType.Sqlite,
+                IsAutoCloseConnection = true,
+                InitKeyType = InitKeyType.SystemTable
+            });
+        }
+
+        private static void Update()
+        {
+        }
+    }
+}
